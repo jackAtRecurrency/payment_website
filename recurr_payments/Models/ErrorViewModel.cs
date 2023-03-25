ed9503860481f5a906d218ecf20761ce554e2f85
jackAtRecurrency <jack@recurrency.com>
2023-03-25 18:03:44 +0800

    test

---
+++ recurr_payments/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace recurr_payments.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
